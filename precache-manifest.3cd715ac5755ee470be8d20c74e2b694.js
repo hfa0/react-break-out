0b1382efb36faaa8446f34efb4ae9ec6f5916dcf
hfa0 <fakhir.husein@gmail.com>
2020-05-24 14:09:49 +0200

    Updates

---
+++ precache-manifest.3cd715ac5755ee470be8d20c74e2b694.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd121b6c2d66b9510e5a",
+    "url": "/react-break-out/static/css/main.54cf7741.chunk.css"
+  },
+  {
+    "revision": "dd121b6c2d66b9510e5a",
+    "url": "/react-break-out/static/js/main.dd121b6c.chunk.js"
+  },
+  {
+    "revision": "38436f265d9e13db806c",
+    "url": "/react-break-out/static/js/1.38436f26.chunk.js"
+  },
+  {
+    "revision": "9d8b0279098736258e12",
+    "url": "/react-break-out/static/js/runtime~main.9d8b0279.js"
+  },
+  {
+    "revision": "784c9efa09f8e9039bec28ec4ced42e7",
+    "url": "/react-break-out/index.html"
+  }
+];
